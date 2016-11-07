dae59261dd12cb1a64f3a7084736c31a4ac0d5ff
Eugene Mirotin <emirotin@gmail.com>
2016-11-07 15:25:10 +0200

    x

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ if (process.env.STOP === '1') {
 var wire = new i2c(BOARD, { device: '/dev/i2c-1' })
 
 function normalizeSpeed(speed) {
-  // the polatiry is inverted, thus `-speed`
+  // the polarity is inverted, thus `-speed`
   var result = -127 + Math.floor(255 * (-speed + 1) / 2)
   console.log('Speed:', speed, 'normalized:', result)
   return result
@@ -21,7 +21,7 @@ function normalizeMotor(motor) {
 
 function setMotor(motor, speed) {
   var value = normalizeSpeed(speed)
-  wire.write([ /*BOARD,*/ normalizeMotor(motor), value ], function(err) {
+  wire.write([ normalizeMotor(motor), value ], function(err) {
     console.error(err)
   })
 }
